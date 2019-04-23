438d4fed55e979d4831cd893dd4addf0e46f4774
Isaiah Morales <misaiah21@yahoo.com>
2019-04-23 14:59:27 -0400

    Add files via upload

--- Bark-n-Bite-master/js/draggable.js
+++ Bark-n-Bite-master/js/draggable.js
@@ -1,5 +1,16 @@
  $(function() 
  {
-    $(".draggable").sortable();
-    $(".draggable").disableSelection();
+    $(".draggable").draggable({helper : "clone"});
+ //   $(".draggable").disableSelection();
+
+    $("#droppable").droppable(
+    	{
+    		accept: ".draggable",
+    		drop: function(ev, ui){
+
+    			var droppedItem = $(ui.draggable).clone();
+    			$(this).append(droppedItem);
+    		}
+    	}
+    );
  });
